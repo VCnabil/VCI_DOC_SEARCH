0f9f6aa92240f5e60f2e0a4d3b2c21043d94a643
Nabil <nlamriben@vcinc.com>
2024-12-12 13:26:22 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace VCI_DOC_SEARCH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkedListBoxPaths = new System.Windows.Forms.CheckedListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SearchBox = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.flowLayoutPanelResults = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Chose a search path root";
+            // 
+            // checkedListBoxPaths
+            // 
+            this.checkedListBoxPaths.FormattingEnabled = true;
+            this.checkedListBoxPaths.Location = new System.Drawing.Point(12, 56);
+            this.checkedListBoxPaths.Name = "checkedListBoxPaths";
+            this.checkedListBoxPaths.Size = new System.Drawing.Size(561, 312);
+            this.checkedListBoxPaths.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(653, 260);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(254, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Chose a search path root";
+            // 
+            // SearchBox
+            // 
+            this.SearchBox.Location = new System.Drawing.Point(657, 288);
+            this.SearchBox.Name = "SearchBox";
+            this.SearchBox.Size = new System.Drawing.Size(249, 31);
+            this.SearchBox.TabIndex = 4;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(658, 325);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(248, 34);
+            this.buttonSearch.TabIndex = 5;
+            this.buttonSearch.Text = "button1";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanelResults
+            // 
+            this.flowLayoutPanelResults.AutoScroll = true;
+            this.flowLayoutPanelResults.BackColor = System.Drawing.Color.PapayaWhip;
+            this.flowLayoutPanelResults.Location = new System.Drawing.Point(12, 374);
+            this.flowLayoutPanelResults.Name = "flowLayoutPanelResults";
+            this.flowLayoutPanelResults.Size = new System.Drawing.Size(934, 793);
+            this.flowLayoutPanelResults.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(2060, 1213);
+            this.Controls.Add(this.flowLayoutPanelResults);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.SearchBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkedListBoxPaths);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox checkedListBoxPaths;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox SearchBox;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelResults;
+    }
+}
+
